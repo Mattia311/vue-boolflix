ae4dcb1e7bd7fbfb3b9fb97d6b020aa263a90893
Mattia311 <mattia.space98@gmail.com>
2021-11-18 10:52:07 +0100

    Structure base

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+   
+  </div>
+</template>
+
+<script>
+
+
+
+export default {
+  name: 'App',
+  components: {
+    
+  }
+}
+</script>
+
+<style lang="scss">
+@import "../node_modules/bootstrap/scss/bootstrap.scss";
+
+</style>
